055062f55f8f981532c8f5777768eaf4d5562af5
Adrian Gałek <galek.adrian.1@gmail.com>
2019-03-29 15:59:54 +0100

    zad2

--- complex.js
+++ complex.js
@@ -1,27 +1,27 @@
-var Complex = /** @class */ (function () {
-    function Complex(real, imaginary) {
+"use strict";
+class Complex {
+    constructor(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    Complex.prototype.add = function (obj) {
+    add(obj) {
         return new Complex(this.real + obj.real, this.imaginary + obj.imaginary);
-    };
-    Complex.prototype.subtract = function (obj) {
+    }
+    subtract(obj) {
         return new Complex(this.real - obj.real, this.imaginary - obj.imaginary);
-    };
-    Complex.prototype.abs = function () {
+    }
+    abs() {
         return Math.sqrt(this.real + this.imaginary);
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         return String(this.real) + " " + String(this.imaginary);
-    };
-    return Complex;
-}());
-var com = new Complex(3, 2);
-var com2 = new Complex(4, 1);
-var com3 = com.add(com2);
+    }
+}
+let com = new Complex(3, 2);
+let com2 = new Complex(4, 1);
+let com3 = com.add(com2);
 console.log(com3);
-var com4 = com.subtract(com2);
+let com4 = com.subtract(com2);
 console.log(com4);
-var com5 = com.abs();
+let com5 = com.abs();
 console.log(com5);
