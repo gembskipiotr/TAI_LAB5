016a6a73ebe2361700e5a788c29771133777875f
gembskipiotr <43858230+gembskipiotr@users.noreply.github.com>
2019-04-12 07:44:53 +0200

    Add files via upload
    Piotr G

---
+++ complex.ts
@@ -0,0 +1,34 @@
+class Complex {
+    private real: number;
+    private imaginary: number;
+
+    constructor(real: number, imaginary: number){
+       this.real = real;
+       this.imaginary = imaginary;  
+    }
+
+    add(object: Complex):Complex{
+        return new Complex(this.real + object.real, this.imaginary + object.imaginary);
+    }
+
+    substract(object: Complex):Complex{
+        return new Complex(this.real - object.real, this.imaginary - object.imaginary);
+    }
+
+    module(){
+        return Math.sqrt((this.real*this.real)+(this.imaginary*this.imaginary));
+    }
+
+    toString(){
+        console.log("Real: "+this.real+", imaginary: "+this.imaginary)
+    }
+}
+
+let com = new Complex(2,3);
+let com2 = new Complex(4,5);
+let com3 = com.add(com2);
+com3.toString();
+let com4 = com3.substract(com);
+com4.toString();
+let modulc = com4.module();
+console.log(modulc);
